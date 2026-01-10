b902f55c686bcbdc91202deb4c6b8fd36e98d21d
Nathaniel James <nathanieltjames24@gmail.com>
2026-01-10 11:12:32 -0500

    add the ads script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import {
   Cedarville_Cursive,
   Dawning_of_a_New_Day,
@@ -82,6 +83,14 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode;}>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6117588691065617"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         className={[
           inter.variable,
